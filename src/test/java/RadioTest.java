694ee53873464fb50fd990a082e35a2414989dbb
CameronTait <cameron-tait@hotmail.com>
2020-06-23 11:33:53 +0100

    can tune into radio station test has passed

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,3 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RadioTest {
+
+    private Radio radio;
+
+    @Before
+    public void before(){
+        radio = new Radio("MFR");
+    }
+
+    @Test
+    public void canTuneToRadioStation(){
+        assertEquals("MFR", radio.tune());
+    }
 
 }
